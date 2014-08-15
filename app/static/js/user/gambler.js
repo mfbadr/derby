91639eb59adc483b7675a74ae604d1215e96bc97
Mikey Badr <mfbadr@gmail.com>
2014-08-15 09:58:44 -0500

    tweaks

--- app/static/js/user/gambler.js
+++ app/static/js/user/gambler.js
@@ -11,27 +11,23 @@
     //debugger;
     var assetName = $(this).children('.assetName').text(),
         gamblerId = $(this).closest('.gambler').data('gamblerId'),
-        url = '/gamblers/' + gamblerId + '/assets/' +assetName;
+        url = '/gamblers/' + gamblerId + '/assets/' +assetName; //pass id and assetname to controller in req.params
 
     //console.log(assetName, gamblerId);
     // url = url, n
     $.ajax({url:url, type:'delete', dataType:'json', success:function(data){
-      console.log(data.id, data.asset);
-      var $asset = $('.assetName:contains('+data.asset+')').closest('.asset');
-      $asset.fadeOut();
-      //var $cash =
-      $('.assetName:contains('+data.asset+')').closest('.gambler').find('.cash').text(data.newCash);
-      var $gambler = $('.gambler[data-gambler-id='+data.id+']');
+      //console.log(data.id, data.asset);
+      var $asset = $('.assetName:contains('+data.asset+')').closest('.asset'); //select asset with correct name
+      $asset.fadeOut(); // fade it out
+      var $gambler = $('.gambler[data-gambler-id='+data.id+']'); //selct that gambler
+      $gambler.find('.cash').text(data.newCash); //update their cash
 
-      //debugger;
-      //$cash;
-      if(data.isDivorced){
-        var $spouse = $gambler.find('.spouse');
-        $spouse.fadeOut();
+      if(data.isDivorced){ //if we got isDivorced
+        var $spouse = $gambler.find('.spouse'); //find the spouse
+        $spouse.fadeOut(); //fade it out
         setTimeout(function(){$spouse.remove();}, 2000);
       }
     }});
   }
-
 })();
 
